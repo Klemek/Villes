fea8def7f62a1dc82863a0d1415dbf069207e3f8
Klemek <Klemek@outlook.fr>
2018-11-21 19:05:43 +0100

    Changed column size

--- src/main/resources/sql/v2.sql
+++ src/main/resources/sql/v2.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `City` (
 	`code` INT NOT NULL,
 	`name` VARCHAR(128) NOT NULL,
-	`postal_codes` VARCHAR(64) NOT NULL,
+	`postal_codes` VARCHAR(128) NOT NULL,
 	`geo_lat` DECIMAL(19,15) NOT NULL,
 	`geo_long` DECIMAL(19,15) NOT NULL,
 	PRIMARY KEY (`code`)
